2114351a26737af09328222576da196561330695
Jason Goebel <goebel.jason@gmail.com>
2016-06-19 23:13:45 -0500

    ADDS:  Linked location to weather works

--- js.js
+++ js.js
@@ -3,30 +3,36 @@
 $(document).ready(function () {
   "use strict";
 
-  function getWeather() {
+  function getWeather(lat, lon) {
     var openWeatherAPI = "http://api.openweathermap.org/data/2.5/weather?",
+      latitude,
+      longitude,
       weatherData;
-    weatherData = $.getJSON(openWeatherAPI, {q: "Kansas City",
+    weatherData = $.getJSON(openWeatherAPI, {lat: lat,
+                                             lon: lon,
                                              appid: "2102d3e0ed083053990492f54bbf38e2"})
       .done(function (data) {
-        $("#data").text("openWeather Data: " + data.coord.lon);
+        $("#data").text("openWeather Data: " + data.name);
+        console.log(data);
       })
       .fail(function (jqxhr, textStatus, error) {
         var err = textStatus + ", " + error;
         $("#data").text("Request Failed: " + err);
       });
-    return weatherData;
   }
 
   function getLocation() {
+    var geo = [];
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position.coords.latitude, position.coords.longitude);
+        getWeather(position.coords.latitude, position.coords.longitude);
       });
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
   }
+
+  getLocation();
 });
 
 
